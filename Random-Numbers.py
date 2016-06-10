38ece9817f806b33974d0c83df1af825a4eb0e7c
Megaphos <connor@quinlan.org>
2016-06-10 18:38:54 -0400

    10 Random numbers printed

---
+++ Random-Numbers.py
@@ -0,0 +1,3 @@
+from random import randint
+Z = [(randint(1,10)),(randint(1,10)),(randint(1,10)),(randint(1,10)),(randint(1,10)),(randint(1,10)),(randint(1,10)),(randint(1,10)),(randint(1,10)),(randint(1,10))]
+print Z
